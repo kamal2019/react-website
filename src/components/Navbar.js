e5b040e0863510aaa7b69d4db694d0c9e239d7cb
kamal aryal <mraryalkamal2011@gmail.com>
2022-02-13 18:34:03 +0545

    to host

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
+      <Link to="/"><img src={Logo} /></Link>
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
           <Link to="/menu"> Menu </Link>
